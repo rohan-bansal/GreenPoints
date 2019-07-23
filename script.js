8177e5467d739db3e68af323be0766672fdf2cb5
Bhavik Joshi <bhavik.joshi28@icloud.com>
2019-07-23 12:12:08 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -3,3 +3,17 @@ function buttonStrike() {
 if (document.getElementById("buttons").checked == true) {
 }
 }
+
+function move() {
+    var elem = document.getElementById("cprogress");   
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+      } else {
+        width++; 
+        elem.style.width = width + '%'; 
+      }
+    }
+  }
